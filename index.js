c515693f5870bc767ae46ef4ac6b78304c9ed483
C.N <cn.mbhalati@gmail.com>
2022-02-05 19:46:25 +0200

    Worked on it for a while

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const container = document.getElementById("container");
+const container = document.querySelector('.container');
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
-};
+let humanChoice = 16
 
-makeRows(16, 16);
+function makeRows(dimension) {
+  for (let c = 0; c < dimension*dimension; c++){
+    cell = document.createElement('div');
+    cell.className = 'grid-item';
+    container.appendChild(cell);
+  }
+}
+makeRows(humanChoice);
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', function(){
+  items.forEach(item => item.classList.remove('hover'))
+  dimensions = prompt('How many dimensions you want?')
+  console.log(dimensions)
+})
+
+const items = document.querySelectorAll('.grid-item');
+items.forEach(item => item.addEventListener('mouseover', function(e) {
+  this.classList.add('hover')
+}))
+
